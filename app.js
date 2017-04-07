ae35e57b11bad828239d53d90ca02cd4839400a5
Pignatar0md <pignataromd@gmail.com>
2017-04-07 11:49:55 -0300

    save status agent

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ var client = new pg.Client({
     if (err) throw err;
     client.query('SELECT id as agente_id from ominicontacto_app_agenteprofile where estado = 2', function (err, result) {
       if (err) throw err;
-      onlineAgents = result.rows;
+      onlineAgents = result.rows.agente_id;
       console.log(onlineAgents);
       client.end(function (err) {
         if (err) throw err;
